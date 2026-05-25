7f076d76401c003bbd7c23009480a0ff21462f8c
insa-ship-it <leemichaelinsa@gmail.com>
2026-05-25 05:18:34 -0500

    Update combine_remote_playlists_with_epg.py

--- combine_remote_playlists_with_epg.py
+++ combine_remote_playlists_with_epg.py
@@ -7,11 +7,11 @@ import re
 PLAYLISTS = [
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/rocket300.m3u8", 
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/ott-news.m3u8", 
+    "https://github.com/insa-ship-it/sports/raw/refs/heads/main/liveeventsfilter.m3u8", 
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/religion.m3u8", 
-    "https://raw.githubusercontent.com/BuddyChewChew/My-Streams/refs/heads/main/Backup.m3u",
-    "https://raw.githubusercontent.com/BuddyChewChew/My-Streams/refs/heads/main/TheTVApp.m3u8",
+    "https://github.com/insa-ship-it/My-Streams/raw/refs/heads/main/Backup.m3u",
+    "https://github.com/insa-ship-it/My-Streams/raw/refs/heads/main/TheTVApp.m3u8",
     "https://raw.githubusercontent.com/BuddyChewChew/buddylive/refs/heads/main/buddylive_v1.m3u", 
-    "https://raw.githubusercontent.com/insa-ship-it/sports/refs/heads/main/liveeventsfilter.m3u8", 
     # Add more playlists here in the format: "URL_TO_PLAYLIST"
 ]
 
